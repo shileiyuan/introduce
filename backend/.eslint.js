adbf44d5f76f2f065ca2ca53383edc79b1199580
ysl <ysl@ysldeMacBook-Pro.local>
2018-08-21 16:26:43 +0800

    提交隐藏文件

---
+++ backend/.eslint.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'eslint-config-egg',
+  rules: {
+    'space-before-function-paren': 0,
+    'comma-dangle': 0,
+    'strict': 0,
+    'semi': [2,  'never']
+  }
+};
---
+++ frontend/.eslint.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['standard', 'standard-react'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true
+  },
+  globals: {
+    $: 'jquery',
+    _: 'lodash',
+    R: 'ramda',
+    cls: 'classnames'
+  },
+  plugins: [
+    'compat'
+  ],
+  rules: {
+    'space-before-function-paren': 0,
+    'react/prop-types': 0,
+    'space-before-function-paren': 'off',
+    'semi': [2,  'never']
+  },
+};
